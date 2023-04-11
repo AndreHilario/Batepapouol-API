bc99d8234873fceba3c987ebe55b1094204471c2
Andre Hilario <andre18hilario@gmail.com>
2023-04-11 09:32:48 -0300

    First commit (App initial structure)

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/participants", (req, res) => {
+    
+});
+app.get("/participants", (req, res) => {
+    
+});
+app.post("/messages", (req, res) => {
+    
+});
+app.get("/messages", (req, res) => {
+    
+});
+app.post("/status", (req, res) => {
+
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`API Bate papouol funcionando na porta ${PORT}`));
