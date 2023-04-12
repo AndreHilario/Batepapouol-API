8022a13581798bc9f9a8fd9fb09a0442ca635126
Andre Hilario <andre18hilario@gmail.com>
2023-04-12 11:01:21 -0300

    Route participants working (missing validations)

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import dayjs from "dayjs";
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 dotenv.config();
 
 
@@ -17,14 +18,29 @@ mongoClient.connect()
     .catch((err) => console.log(err.message))
 
 
+app.post("/participants", async (req, res) => {
+    try {
+        const { name } = req.body;
 
-app.post("/participants", (req, res) => {
+        const now = dayjs(); //Data atual
+        const currentTime = now.format('HH:mm:ss'); //Data no formato correto
 
-    const { name } = req.body;
-    
+        const newUser = { name, lastStatus: Date.now() };
+        const newMessage = { from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: currentTime };
 
+        await db.collection("participants").insertOne(newUser);
+        await db.collection("messages").insertOne(newMessage);
+
+        res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 });
 app.get("/participants", (req, res) => {
+
+    db.collection("participants").find().toArray()
+        .then(participants => res.send(participants))
+        .catch(err => res.status(500).send(err.message))
 
 });
 
@@ -44,4 +60,4 @@ app.post("/status", (req, res) => {
 });
 
 const PORT = 5000;
-app.listen(PORT, () => console.log(`API Batepapo uol funcionando na porta ${PORT}`));
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
