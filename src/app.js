a5e1a4f846d301758f769c1a1f2d53998c3f8dc4
Andre Hilario <andre18hilario@gmail.com>
2023-04-14 12:33:08 -0300

    Last version working

--- src/app.js
+++ src/app.js
@@ -4,14 +4,10 @@ import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 import dayjs from "dayjs";
 import Joi from "joi";
-import { stripHtml } from "string-strip-html";
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 dotenv.config();
-
 // Conectando MongoDB com async/await
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 try {
@@ -21,62 +17,33 @@ try {
     console.log(err.message);
 }
 const db = mongoClient.db()
-
 const currentDate = dayjs(); //Data atual
 const currentTime = currentDate.format('HH:mm:ss'); //Data no formato correto
-
-
 async function checkLoggedUser() {
     const timeStatus = Date.now() - 10000;
-    const sanitizedText = stripHtml("sai da sala...").trim();
-
     const userToBeDeleted = await db.collection("participants").findOne({ lastStatus: { $lt: timeStatus } });
-
-    const sanitizedName = stripHtml(userToBeDeleted.name).trim();
-
     if (userToBeDeleted) {
-        const removedUser = { from: sanitizedName, to: "Todos", text: sanitizedText, type: "status", time: currentTime };
-
+        const removedUser = { from: userToBeDeleted.name, to: 'Todos', text: 'sai da sala...', type: 'status', time: currentTime };
         await db.collection("participants").deleteOne({ _id: userToBeDeleted._id });
         await db.collection("messages").insertOne(removedUser);
         console.log(`Usuário ${userToBeDeleted.name} removido`);
     }
 };
-
 setInterval(checkLoggedUser, 15000);
-
-
 app.post("/participants", async (req, res) => {
-
     const { name } = req.body;
-    const sanitizedName = stripHtml(name).trim();
-    const newText = stripHtml("entra na sala...").trim();
     const userSchema = Joi.object({
         name: Joi.string().required()
     });
-
     const { error } = userSchema.validate(req.body);
-
     if (error) return res.sendStatus(422);
-
     try {
-
         const searchUsers = await db.collection("participants").find({ name }).toArray()
-
         if (searchUsers.length > 0) return res.sendStatus(409);
-
-        const newUser = { name: sanitizedName, lastStatus: Date.now() };
-        const newMessage = { 
-            from: sanitizedName, 
-            to: "Todos", 
-            text: newText, 
-            type: "status", 
-            time: currentTime 
-        };
-
+        const newUser = { name, lastStatus: Date.now() };
+        const newMessage = { from: name, to: "Todos", text: "entra na sala...", type: "status", time: currentTime };
         await db.collection("participants").insertOne(newUser);
         await db.collection("messages").insertOne(newMessage);
-
         res.sendStatus(201);
     } catch (err) {
         res.status(500).send(err.message);
@@ -84,78 +51,48 @@ app.post("/participants", async (req, res) => {
 });
 app.get("/participants", async (req, res) => {
     try {
-        const allParticipants = await db.collection("participants").find().toArray();
+        const allParticipants = await db.collection("participants").find().toArray()
         res.send(allParticipants);
     } catch (err) {
         res.status(500).send(err.message);
     }
-
 });
-
 app.post("/messages", async (req, res) => {
-
     const { to, text, type } = req.body;
     const { user } = req.headers;
-
-    const sanitizedUSer = stripHtml(user).trim();
-    const sanitizedText = stripHtml(text).trim();
-
-    function verifyNewBody(req) {
+    function verifyBody(req) {
         if (req.body) {
             return { ...req.body, from: user }
         }
-
         return req;
     }
-
-    const newBody = verifyNewBody(req);
-
+    const newBody = verifyBody(req);
     const messageBodySchema = Joi.object({
         from: Joi.string().required(),
         to: Joi.string().required(),
         text: Joi.string().required(),
         type: Joi.string().valid("message", "private_message").required()
     });
-
     const { error: errorBody } = messageBodySchema.validate(newBody);
-
     if (errorBody) return res.status(422).send(errorBody.message);
-
     try {
-
         const userFromMessage = await db.collection("messages").find({ from: user }).toArray()
-
         if (userFromMessage.length === 0) return res.status(422).send("Usuário remetente não existe");
-
-
-        const sendMessage = { 
-            from: sanitizedUSer, 
-            to, 
-            text: sanitizedText, 
-            type, 
-            time: currentTime 
-        };
-
+        const sendMessage = { from: user, to, text, type, time: currentTime };
         await db.collection("messages").insertOne(sendMessage);
-
         res.sendStatus(201);
     } catch (err) {
         res.status(500).send(err.message);
     }
-
 });
 app.get("/messages", async (req, res) => {
-
     const { limit } = req.query;
     const { user } = req.headers;
-
     const limitSchema = Joi.number()
         .integer()
         .min(1)
         .required();
-
     try {
-
         const searchMessages = await db.collection("messages").find
             ({
                 $or: [
@@ -166,11 +103,8 @@ app.get("/messages", async (req, res) => {
                     { type: "status", to: "Todos" }
                 ]
             }).toArray()
-
         if (limit) {
-
             const { error: errorLimit } = limitSchema.validate(limit);
-
             if (errorLimit) {
                 res.status(422).send(errorLimit.message)
             } else {
@@ -182,48 +116,30 @@ app.get("/messages", async (req, res) => {
     } catch (err) {
         res.status(500).send(err.message);
     }
-
 });
 app.post("/status", async (req, res) => {
-
     const { user } = req.headers;
-    const sanitizedUSer = stripHtml(user).trim();
-
     if (!user) return res.sendStatus(404);
-
     try {
-
         const findNewUser = await db.collection("participants").find({ name: user }).toArray()
-
         if (findNewUser.length === 0) return res.sendStatus(404);
-
-        const refreshedUser = { name: sanitizedUSer, lastStatus: Date.now() };
-
+        const refreshedUser = { name: user, lastStatus: Date.now() };
         await db.collection("participants").insertOne(refreshedUser);
         console.log(refreshedUser)
-
         res.sendStatus(200)
     } catch (err) {
         res.status(500).send(err.message);
     }
 });
-
 app.delete("/messages/:id", async (req, res) => {
-
     const { id } = req.params;
     const { user } = req.headers;
-
     if (!id || !user) return res.status(422).send("User required on header and id on path params")
-
     try {
         const result = await db.collection("messages").findOne({ _id: new ObjectId(id) });
-
         if (!result) return res.status(404).send("Message not found");
-
         if (result.from !== user) return res.status(401).send("Unauthorized");
-
         await db.collection("messages").deleteOne({ _id: new ObjectId(id) });
-
         res.status(200).send("Message deleteds with sucess");
     } catch (err) {
         res.status(500).send(err.message);
@@ -236,18 +152,15 @@ app.put("/messages/:id"), async (req, res) => {
     const { user } = req.headers;
     const { to, text, type } = req.body;
 
-    const sanitizedUSer = stripHtml(user).trim();
-    const sanitizedText = stripHtml(text).trim();
-
     const editedMessage = {};
 
     if (to) editedMessage.to = to;
-    if (text) editedMessage.text = sanitizedText;
+    if (text) editedMessage.text = text;
     if (type) editedMessage.type = type;
 
     function verifyBody(req) {
         if (req.body) {
-            return { ...req.body, from: sanitizedUSer }
+            return { ...req.body, from: user }
         }
 
         return req;
